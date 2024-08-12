dd9b67f286115e3a4257b680fdb6e8079943f4ab
chikityin <>
2024-08-12 16:00:27 -0700

    Created Project

---
+++ src/main/java/org/example/chikityin_300380394_finalexam/ChiKitYin300380394FinalexamApplication.java
@@ -0,0 +1,13 @@
+package org.example.chikityin_300380394_finalexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiKitYin300380394FinalexamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChiKitYin300380394FinalexamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/chikityin_300380394_finalexam/ChiKitYin300380394FinalexamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chikityin_300380394_finalexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiKitYin300380394FinalexamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
